dd5ba4fe9c63fbf395859f100a5fb89bd846290f
Erol Cetinok <erol.cetinok@gmail.com>
2026-05-20 16:44:18 -0700

    starting on IK stuff

--- src/quadruped/kinematics/ik.py
+++ src/quadruped/kinematics/ik.py
@@ -5,3 +5,9 @@ Implement here. Downstream code that imports from this module
 will `ImportError` until you re-add your public symbols and re-export them in
 `kinematics/__init__.py`.
 """
+
+import numpy as np
+from quadrupued.kinematics.fk import L1, L2, L3
+
+def joint_angles (x: float, y: float, z: float) -> [float,float,float]:
+
