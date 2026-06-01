2f0997de8bdb62f18ae5ddf5a1b9b98dbdb7b100
Erol Cetinok <erol.cetinok@gmail.com>
2026-05-31 20:21:09 -0700

    working on adjusting body config

--- src/quadruped/config.py
+++ src/quadruped/config.py
@@ -105,12 +105,12 @@ Joint(name='wing_BR', leg='BR', joint='wing', motor_id=11, limit_rad=(-0.8726646
 Joint(name='knee_BR', leg='BR', joint='knee', motor_id=12, limit_rad=(-2.00712863979, 1.57079632679), offset_deg=180.000000, direction=1),
 ),
     links_mm=LinksMM(
-        shoulder_to_wing=(-20.25, -23.25, 21.4),
+        shoulder_to_wing=(0.0, -38.25, 21.4),
         wing_to_knee=(0.0, -107.5, -23.25),
         knee_to_foot=(21.0, 0.0, -110.0),
     ),
     foot_site_offset_mm=(21, -12, 0),
-    target_site_offset_mm=(20, -175, -50),
+    target_site_offset_mm=(0, -175, -50),
     mjcf=MjcfNames(
         foot_site_name='foot_site',
         target_site_name='target_site',
